3ee00633d571cad73f7d3e48dfe8f0131c22ea39
Alan Hernandez <alan.m.hernandez@gmail.com>
2017-10-28 18:47:46 -0400

    Fixed Weather Data Api and button

--- js/script.js
+++ js/script.js
@@ -1,92 +1,71 @@
 $(document).ready(function() {
   // Get Location based on IP
-  $.getJSON("http://ip-api.com/json/", function(data) {
-    $("#location").html(data.city);
-    // Get Zip and Country Code and store them in variables
-    var zip = data.zip;
-    var countryCode = data.countryCode.toLowerCase();
+  var latitude, longitude;
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(populateData);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+  }
+  function populateData(position) {
+    latitude = position.coords.latitude.toFixed(4);
+    longitude = position.coords.longitude.toFixed(4);
+    $("#latitude .coordinates").html(latitude);
+    $("#longitude .coordinates").html(longitude);
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + Math.round(latitude) + "&lon=" + Math.round(longitude), function(data) {
+      temp = Math.round((data.main.temp * 9)/5 + 32);
+      $('#temperature').html(temp + '\xB0F');
 
-    // Get Weather, Temperature, and wind using OpenWeatherMap API, zip code, and country code
+      $("#weather").html(data.weather[0].main);
 
-    getWeather(zip, countryCode);
+      var windSpeed = data.wind.speed;
+      var windDirection = data.wind.deg;
 
-  });
+      // Changing the value of windDirection to an actual direction
+      if(windDirection > 345 || windDirection <= 30) {
+        windDirection = "E";
+      }
+      else if (windDirection > 30 && windDirection <= 75) {
+        windDirection = "NE";
+      }
+      else if (windDirection > 75 && windDirection <= 105) {
+        windDirection = "N";
+      }
+      else if (windDirection > 105 && windDirection <= 165) {
+        windDirection = "NW";
+      }
+      else if (windDirection > 165 && windDirection <= 195) {
+        windDirection = "W";
+      }
+      else if (windDirection > 195 && windDirection <= 245) {
+        windDirection = "SW";
+      }
+      else if (windDirection > 245 && windDirection <= 285) {
+        windDirection = "S";
+      }
+      else if (windDirection > 285 && windDirection <= 345) {
+        windDirection = "SE";
+      }
+      if(windSpeed == null) {
+        windSpeed = 'N/A Wind Speed';
+        console.log("Failed to get wind speed");
+      }
+      if(windDirection == null) {
+        windDirection = '';
+        console.log("Failed to get wind direction");
+      }
+      $("#wind").html(windDirection + " " + windSpeed + " knots");
 
-  function getWeather(zip, countryCode) {
-    var api_key = '8eda145370e8903509870ccfbb9e452f';
-    $.ajax({
-      url : "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + countryCode + '&mode=html&appid=' + api_key,
-      type: 'GET',
-      dataType: 'jsonp',
-      success : function(weatherData) {
-        // Temperature
-        var temperature = Math.round((weatherData.main.temp * 9)/5 + 32);
-        $("#temperature").html(temperature + '\xB0F');
+      // Change Page Color according to Weather
+      if(data.weather[0].main == 'Rain') {
 
-        // Weather
-        var forecast = weatherData.weather[0].main;
-        if(forecast == null) {
-          $("#weather").html("N/A");
-          console.log("Failed to get weather information");
-        }
-        else {
-          $("#weather").html(forecast);
-        }
-
-        // Winds
-
-        var windSpeed = weatherData.wind.speed;
-        var windDirection = weatherData.wind.deg;
-        // Changing the value of windDirection to an actual direction
-        if(windDirection > 345 || windDirection <= 30) {
-          windDirection = "E";
-        }
-        else if (windDirection > 30 && windDirection <= 75) {
-          windDirection = "NE";
-        }
-        else if (windDirection > 75 && windDirection <= 105) {
-          windDirection = "N";
-        }
-        else if (windDirection > 105 && windDirection <= 165) {
-          windDirection = "NW";
-        }
-        else if (windDirection > 165 && windDirection <= 195) {
-          windDirection = "W";
-        }
-        else if (windDirection > 195 && windDirection <= 245) {
-          windDirection = "SW";
-        }
-        else if (windDirection > 245 && windDirection <= 285) {
-          windDirection = "S";
-        }
-        else if (windDirection > 285 && windDirection <= 345) {
-          windDirection = "SE";
-        }
-        if(windSpeed == null) {
-          windSpeed = 'N/A Wind Speed';
-          console.log("Failed to get wind speed");
-        }
-        if(windDirection == null) {
-          windDirection = '';
-          console.log("Failed to get wind direction");
-        }
-        $("#wind").html(windDirection + " " + windSpeed + " knots");
-      },
-      error: function () {
-        console.log('Failed!');
       }
     });
   }
+  getLocation();
 
-  // function changeCloudColor() {
-  // }
-  //
-  // function changePageColor() {
-  // }
-
-
-
-});
+ });
 
 $('#changeDegreeUnits').click(function() {
   var fullText = $('#temperature').text();
@@ -101,3 +80,11 @@ $('#changeDegreeUnits').click(function() {
     $('#temperature').text(temp + '\xB0F');
   }
 });
+
+
+  // function changeCloudColor() {
+  // }
+  //
+  // function changePageColor() {
+  // }
+
